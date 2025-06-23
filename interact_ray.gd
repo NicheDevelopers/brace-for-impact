376fa717c8f0755158434bb3bcdae976eec99b92
Michał Miłek <michal.mileq@gmail.com>
2025-06-23 17:11:00 +0200

    Basic interaction system

--- interact_ray.gd
+++ interact_ray.gd
@@ -3,8 +3,14 @@ extends RayCast3D
 @onready var prompt: Label = $Prompt
 
 func _physics_process(delta: float) -> void:
-	var prompt_text := ""
+	prompt.text = ""
 	
 	if is_colliding():
 		var collider := get_collider()
-		prompt.text = collider.name
+		
+		if collider is Interactable:
+			if collider.is_enabled:
+				prompt.text = collider.get_prompt()
+				
+				if Input.is_action_just_pressed(collider.interact_action_name):
+					collider.interact(self.owner) # Passes the parent Player node
---
+++ src/common/Interactable/Interactable.gd
@@ -0,0 +1,20 @@
+extends CollisionObject3D
+
+class_name Interactable
+
+signal interacted(body)
+
+@export var prompt_message: String = "Interact"
+@export var is_enabled: bool = true
+@export var interact_action_name: String = "interact"
+
+func interact(body):
+	interacted.emit(body)
+	
+func get_prompt() -> String:
+	var key_name := ""
+	for action in InputMap.action_get_events(self.interact_action_name):
+		if action is InputEventKey:
+			key_name = action.as_text_physical_keycode()
+			break
+	return prompt_message + "\n[" + key_name + "]"
