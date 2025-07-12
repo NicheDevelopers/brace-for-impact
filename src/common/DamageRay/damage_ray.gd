1ffe96ed296e9db8dcae647cd73277aacf11cb0d
Michał Miłek <michal.mileq@gmail.com>
2025-07-13 00:36:22 +0200

    Hotfix

--- src/common/DamageRay/damage_ray.gd
+++ src/common/DamageRay/damage_ray.gd
@@ -17,11 +17,7 @@ func trigger(by_who: Variant):
 				var health = collider.get_node("HealthComponent")
 				health.damage(damage_dealt, by_who)
 				return
-		if debug_mode:
-			print(collider)
 		collider = collider.get_parent()
-		#collider.get_tree()
-		#get_tree().get_roo
 		if collider.get_parent() == null:
 			# Nothing with a health component was hit
 			return
