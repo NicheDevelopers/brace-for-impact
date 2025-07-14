40df361809cec80bec4fae1bd83141fd0c657f74
Artur Gulik <artur.gulik@outlook.com>
2025-07-14 22:57:46 +0200

    Add WallBox

---
+++ src/MapGenerator/parts/decorations/panels/WallBox/wall_box.gd
@@ -0,0 +1,18 @@
+extends Interactable
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+enum WallBoxState {
+	OPEN,
+	CLOSED
+}
+
+var state := WallBoxState.CLOSED
+
+func _on_interacted(body: Variant) -> void:
+	if (state == WallBoxState.CLOSED):
+		animation_player.play("open")
+		state = WallBoxState.OPEN
+		return
+	animation_player.play("close")
+	state = WallBoxState.CLOSED
